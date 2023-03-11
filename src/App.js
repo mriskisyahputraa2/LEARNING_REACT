aedd0686f9d68a6c664ff0fc741ce9046a1863a9
mriskisyahputraa2 <riskideveloper2@gmail.com>
2023-03-11 15:05:52 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
 import "./index.css";
 import Employee from "./components/Employee";
 import { useState } from "react";
-import Img from './assets/img/gbr6.jpg'
+import {v4 as uuidv4} from 'uuid'; // import id unik dengan menginstall npm install uuid
+
 
 function App() {
-  const [role, setRole] = useState("dev");
-  const [age, setAge] = useState(14);
 
-  const Age = (e) => {
-    setAge(e.target.value);
-  };
-  <Employee name="Rahmat" />;
+  const [employees, setEmployees] = useState([
+    { name: "Jhon", role: "Developer", img: "https://images.pexels.com/photos/15790182/pexels-photo-15790182.jpeg?auto=compress&cs=tinysrgb&w=600" },
+    { name: "Annise", role: "Developer", img: "https://images.pexels.com/photos/15126611/pexels-photo-15126611.jpeg?auto=compress&cs=tinysrgb&w=600" },
+    { name: "Clara", role: "Developer", img: "https://images.pexels.com/photos/15787375/pexels-photo-15787375.jpeg?auto=compress&cs=tinysrgb&w=600" },
+    { name: "Benjamin", role: "Developer", img: "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=600" },
+    { name: "Alex", role: "Developer", img: "https://images.pexels.com/photos/2379004/pexels-photo-2379004.jpeg?auto=compress&cs=tinysrgb&w=600" },
+  ]);
 
   return (
     <>
       <div className="flex flex-wrap justify-center">
-        <Employee name="Muhammad" role={role} age={age} Img={Img}/>
-        <Employee name="Muhammad" role={role} age={age} Img={Img}/>
-        <Employee name="Muhammad" role={role} age={age} Img={Img}/>
-        <Employee name="Muhammad" role={role} age={age} Img={Img}/>
-        <Employee name="Muhammad" role={role} age={age} Img={Img}/>
-       
+        {employees.map((employee) => {
+          console.log(uuidv4());
+            return (
+              <Employee 
+                key={uuidv4()}
+                name={employee.name}
+                role={employee.role}
+                img={employee.img}
+              />
+            )
+        })}
       </div>
     </>
   );
