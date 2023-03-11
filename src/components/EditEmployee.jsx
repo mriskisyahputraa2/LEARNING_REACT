837c57800f971e43ebf5f260dae89017e5ace682
mriskisyahputraa2 <riskideveloper2@gmail.com>
2023-03-11 16:40:30 +0700

    update-otodidak

--- src/components/EditEmployee.jsx
+++ src/components/EditEmployee.jsx
@@ -21,35 +21,36 @@ function EditEmployee() {
                 keyboard={false}
             >
                 <Modal.Header closeButton>
-                    <Modal.Title>Modal title</Modal.Title>
+                    <Modal.Title>Update Employee</Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
-                    <form className="w-full max-w-sm" >
+                    <form id="editmodel" className="w-full max-w-sm">
                         <div className="md:flex md:items-center mb-6">
                             <div className="md:w-1/3">
                                 <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="name">
                                     Full Name
                                 </label>
-                                <div class="md:w-2/3">
-                                <input class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="name" type="text" value="Jane Doe"/>
-                                </div>
                             </div>
+                            <div className="md:w-2/3">
+                                <input className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="name" type="text" value="Jane Doe"/>
                             </div>
-                            <div className="md:flex md:items-center">
-                                <div className="md:w-1/3"></div>
-                                <div className="md:w-2/3">
-                                    <button className="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" type="button">
-                                        Sign Up
-                                    </button>
-                                </div>
+                        </div>
+                        <div className="md:flex md:items-center mb-6">
+                            <div className="md:w-1/3">
+                                <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="role">
+                                    Role
+                                </label>
                             </div>
-                        </form>
+                            <div className="md:w-2/3">
+                                <input className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="role" type="text" value="Jane Doe"/>
+                            </div>
+                        </div>
+                      
+                    </form>
                 </Modal.Body>
                 <Modal.Footer>
-                    <Button variant="secondary" onClick={handleClose}>
-                        Edit
-                    </Button>
-                    <Button variant="primary">Update</Button>
+                    <button onClick={handleClose} className='bg-slate-500 hover:bg-slate-700 text-white font-bold py-2 px-4 rounded'>Close</button>
+                    <button form='editmodel' className='bg-purple-600 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded'>Update</button>
                 </Modal.Footer>
             </Modal>
         </>
