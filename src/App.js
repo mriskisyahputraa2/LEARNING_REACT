9284f1e0d772214b8195125a7a4678709b0d0379
mriskisyahputraa2 <riskideveloper2@gmail.com>
2023-03-11 18:36:43 +0700

    form update complate

--- src/App.js
+++ src/App.js
@@ -7,31 +7,54 @@ function App() {
   const [employees, setEmployees] = useState(
     [
     {
+      id:1,
       name: "Jhon",
       role: "Developer",
       img: "https://images.pexels.com/photos/15790182/pexels-photo-15790182.jpeg?auto=compress&cs=tinysrgb&w=600",
     },
     {
+      id:2,
       name: "Annise",
       role: "Developer",
       img: "https://images.pexels.com/photos/15126611/pexels-photo-15126611.jpeg?auto=compress&cs=tinysrgb&w=600",
     },
     {
+      id:3,
       name: "Clara",
       role: "Developer",
       img: "https://images.pexels.com/photos/15787375/pexels-photo-15787375.jpeg?auto=compress&cs=tinysrgb&w=600",
     },
     {
-      name: "Benjamin",
-      role: "Developer",
-      img: "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=600",
-    },
-    {
+      id:4,
       name: "Alex",
       role: "Developer",
       img: "https://images.pexels.com/photos/2379004/pexels-photo-2379004.jpeg?auto=compress&cs=tinysrgb&w=600",
     },
+    {
+      id:5,
+      name: "Annie",
+      role: "Developer",
+      img: "https://images.pexels.com/photos/1727273/pexels-photo-1727273.jpeg?auto=compress&cs=tinysrgb&w=600",
+    },
+    {
+      id:6,
+      name: "Annie",
+      role: "Developer",
+      img: "https://images.pexels.com/photos/1727273/pexels-photo-1727273.jpeg?auto=compress&cs=tinysrgb&w=600",
+    },
   ]);
+
+  function updateEmployee(id, newName, newRole) {
+    const updatedEmployees = employees.map((employee) => {
+      if(id == employee.id){
+        return {...employee, name:newName, role:newRole}
+      }  
+
+      return employee;
+    });
+    setEmployees(updatedEmployees)
+  }
+
 
   return (
     <>
@@ -39,10 +62,12 @@ function App() {
         {employees.map((employee) => {
           return (
             <Employee
-              key={uuidv4}
+              key={employee.id}
+              id={employee.id}
               name={employee.name}
               role={employee.role}
               img={employee.img}
+              updateEmployee={updateEmployee}
             />
           );
         })}
