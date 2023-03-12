60e083876f7b817b1f8a1a4d4986624c15191e1b
mriskisyahputraa2 <riskideveloper2@gmail.com>
2023-03-12 11:55:10 +0700

    create addEmployee

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./index.css";
 import Employee from "./components/Employee";
 import { useState } from "react";
 import { v4 as uuidv4 } from "uuid"; // import id unik dengan menginstall npm install uuid
+import AddEmployee from "./components/AddEmployee";
 
 function App() {
   const [employees, setEmployees] = useState(
@@ -72,8 +73,10 @@ function App() {
           );
         })}
       </div>
+      <AddEmployee/>
     </>
   );
 }
 
 export default App;
+ 
